2dfda7ab13b418645b52ff9eead57347c91929bd
Jon Borgonia <jon@gomagames.com>
2015-10-10 11:46:39 -1000

    adds games and players collection. creates game and goes to lobby

--- game/client/main.js
+++ game/client/main.js
@@ -20,3 +20,88 @@ Template.startMenu.helpers({
 Template.startMenu.rendered = function () {
 
 };
+
+
+Template.createMeeting.events({
+  'submit #create-meeting': function (event) {
+
+    var playerName = event.target.playerName.value;
+
+    if (!playerName) {
+      return false;
+    }
+
+    var game = generateNewGame();
+    var player = generateNewPlayer(game, playerName);
+
+    Meteor.subscribe('games', game.accessCode);
+
+    Session.set("loading", true);
+
+    Meteor.subscribe('players', game._id, function onReady(){
+      Session.set("loading", false);
+
+      Session.set("gameID", game._id);
+      Session.set("playerID", player._id);
+      Session.set("currentView", "lobby");
+    });
+
+    return false;
+  },
+  'click .btn-back': function () {
+    Session.set("currentView", "startMenu");
+    return false;
+  }
+});
+
+Template.createMeeting.helpers({
+  isLoading: function() {
+    return Session.get('loading');
+  }
+});
+
+Template.createMeeting.rendered = function (event) {
+  $("#player-name").focus();
+};
+
+function generateAccessCode(){
+  var code = "";
+  var possible = "abcdefghijklmnopqrstuvwxyz";
+
+    for(var i=0; i < 6; i++){
+      code += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return code;
+}
+
+function generateNewGame(){
+  var game = {
+    accessCode: generateAccessCode(),
+    state: "waitingForPlayers",
+    location: null,
+    lengthInMinutes: 8,
+    endTime: null,
+    paused: false,
+    pausedTime: null
+  };
+
+  var gameID = Games.insert(game);
+  game = Games.findOne(gameID);
+
+  return game;
+}
+
+function generateNewPlayer(game, name){
+  var player = {
+    gameID: game._id,
+    name: name,
+    role: null,
+    isSpy: false,
+    isFirstPlayer: false
+  };
+
+  var playerID = Players.insert(player);
+
+  return Players.findOne(playerID);
+}
--- game/server/main.js
+++ game/server/main.js
@@ -1,3 +1,14 @@
-Meteor.startup(function () {
 
+Meteor.startup(function () {
+  // Delete all games and players at startup
+  Games.remove({});
+  Players.remove({});
+});
+
+Meteor.publish('games', function(accessCode) {
+  return Games.find({"accessCode": accessCode});
+});
+
+Meteor.publish('players', function(gameID) {
+  return Players.find({"gameID": gameID});
 });
