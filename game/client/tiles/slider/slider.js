959a549ebdf5cfa73ebac827c375bdd71f014625
Emmanuel Pilande <manny@sudokrew.com>
2015-10-11 01:26:01 -1000

    Update Current value to range slider value

--- game/client/tiles/slider/slider.js
+++ game/client/tiles/slider/slider.js
@@ -1,6 +1,16 @@
 Template.slider.events({
-  'click .action' : function(){
+  'click .action': function() {
     performAction( this.label, Template.parentData(2).data.title );
+  },
+  'change .range-slider': function(event) {
+     var sliderValue = event.currentTarget.value
+     Session.set('sliderValue', sliderValue)
   }
+});
 
+Template.slider.helpers({
+  current: function() {
+    var currentValue = Session.get('sliderValue') || 1;
+    return currentValue;
+  }
 });
