758d9d8e2a467b9d3afcee604a8140eba1eaa965
Emmanuel Pilande <manny@sudokrew.com>
2015-10-11 04:25:10 -1000

    If less than 3 buttons display buttons as column layout

--- game/client/tiles/button/button.js
+++ game/client/tiles/button/button.js
@@ -1,5 +1,19 @@
+Template.button.created = function() {
+  this.options = this.data.options;
+};
+
 Template.button.events({
   'click .action' : function(){
     performAction( this.label, Template.parentData(2).data.title );
+  }
+});
+
+Template.button.helpers({
+  buttonLayout: function() {
+    var options = Template.instance().options;
+    if (options.length > 3) {
+      return 'wrap';
+    }
+    return 'column';
   }
 });
