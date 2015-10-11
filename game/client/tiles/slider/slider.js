ac0e8765f5ae62151fe8d1ea5babf5f7450fc058
Emmanuel Pilande <manny@sudokrew.com>
2015-10-11 01:52:05 -1000

    Bind slider Current value specific to template

--- game/client/tiles/slider/slider.js
+++ game/client/tiles/slider/slider.js
@@ -1,16 +1,20 @@
+Template.slider.created = function() {
+  this.sliderValue = new ReactiveVar(1);
+};
+
 Template.slider.events({
   'click .action': function() {
     performAction( this.label, Template.parentData(2).data.title );
   },
   'change .range-slider': function(event) {
-     var sliderValue = event.currentTarget.value
-     Session.set('sliderValue', sliderValue)
+     var sliderValue = event.currentTarget.value;
+     Template.instance().sliderValue.set(sliderValue);
   }
 });
 
 Template.slider.helpers({
   current: function() {
-    var currentValue = Session.get('sliderValue') || 1;
+    var currentValue = Template.instance().sliderValue.get();
     return currentValue;
   }
 });
